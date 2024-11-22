7fee163288caaa4168dedbc1664afc2b6a1744fb
Zabzul Supratman <zabzuls@gmail.com>
2024-11-22 15:13:20 +0700

    [feature] improve navbar

--- components/Navbar/NavbarComponent.tsx
+++ components/Navbar/NavbarComponent.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "flowbite-react";
 
 export function NavbarComponent() {
   return (
-    <Navbar className=" bg-indigo-500" fluid rounded>
+    <Navbar fluid rounded>
       <Navbar.Brand as={Link} href="/">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white"> Zabzul Supratman</span>
       </Navbar.Brand>
